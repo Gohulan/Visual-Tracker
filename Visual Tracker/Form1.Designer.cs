7ae2f194585aa57901d6797b8499ca6aab414a5f
Gohulan Somanathan <gohulan@outlook.com>
2023-06-28 16:02:43 +0530

    Add project files.

---
+++ Visual Tracker/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Visual_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnOpenCamera = new System.Windows.Forms.Button();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtProductCode = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(678, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 149);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "FIND CODE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnOpenCamera
+            // 
+            this.btnOpenCamera.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenCamera.Location = new System.Drawing.Point(678, 37);
+            this.btnOpenCamera.Name = "btnOpenCamera";
+            this.btnOpenCamera.Size = new System.Drawing.Size(217, 149);
+            this.btnOpenCamera.TabIndex = 2;
+            this.btnOpenCamera.Text = "SAVE NEW ITEM";
+            this.btnOpenCamera.UseVisualStyleBackColor = true;
+            this.btnOpenCamera.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(678, 228);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(217, 20);
+            this.txtProductName.TabIndex = 3;
+            // 
+            // txtProductCode
+            // 
+            this.txtProductCode.Location = new System.Drawing.Point(678, 290);
+            this.txtProductCode.Name = "txtProductCode";
+            this.txtProductCode.Size = new System.Drawing.Size(217, 20);
+            this.txtProductCode.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 293);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(675, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(675, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Product Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(21, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "GOHUL\'S VISUAL TRACKER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtProductCode);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.btnOpenCamera);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Visual Tracker - S GOHULAN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnOpenCamera;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtProductCode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Visual Tracker/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Visual_Tracker
+{
+    public partial class Form1 : Form
+    {
+        private bool isCameraOpen = false;
+        private string savedProductName;
+        private string savedProductCode;
+        private VideoCaptureDevice videoSource;
+        private FilterInfoCollection videoDevices;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count == 0)
+            {
+                MessageBox.Show("No video devices found.");
+                return;
+            }
+
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+
+            // Start capturing from the video source
+            videoSource.Start();
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Display the captured frame in the PictureBox control
+            Bitmap frame = (Bitmap)eventArgs.Frame.Clone();
+
+            // Update the PictureBox on the UI thread
+            pictureBox1.Invoke((MethodInvoker)(() =>
+            {
+                pictureBox1.Image = frame;
+            }));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Capture the current frame and save it as an image with the product name and product code
+            if (pictureBox1.Image != null && !string.IsNullOrEmpty(txtProductName.Text) && !string.IsNullOrEmpty(txtProductCode.Text))
+            {
+                // Create the directory if it doesn't exist
+                string directoryPath = @"C:\products";
+                Directory.CreateDirectory(directoryPath);
+
+                // Generate the file name using the product name and product code
+                string productName = txtProductName.Text;
+                string productCode = txtProductCode.Text;
+                string fileName = $"{productName}_{productCode}.jpg";
+                string filePath = Path.Combine(directoryPath, fileName);
+
+                // Save the image
+                pictureBox1.Image.Save(filePath);
+                MessageBox.Show("Image captured and saved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a product name and product code before capturing.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if the product exists in the saved product folder
+            string directoryPath = @"C:\products";
+            string productName = txtProductName.Text;
+            string productCode = txtProductCode.Text;
+            string fileName = $"{productName}_{productCode}.jpg";
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            if (File.Exists(filePath))
+            {
+                MessageBox.Show("Product exists.");
+            }
+            else
+            {
+                MessageBox.Show("Product does not exist.");
+            }
+        }
+    }
+}
+
+
